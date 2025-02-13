d58e70c9e1d10b0885061438cf67ab13baa8afcf
unknown <ktoufiquee@gmail.com>
2025-02-13 13:57:22 -0800

    Prints Something

--- HelloWorld.py
+++ HelloWorld.py
@@ -1,13 +1,12 @@
 print("Hello World")
 print("Hello World")
 
+print("Something")
+print("Hello World")
+print("Something")
 
 print("Hello World")
-
-
-print("Hello World")
-
-
+print("Something")
 print("Hello World")
 
 
